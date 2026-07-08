fd004d7759dfb11ef88d4488478764bb6c9e2b94
Soufiane TAOUIQ <161170101+TAOUFIQ95@users.noreply.github.com>
2026-07-08 15:42:35 +0200

    Add @NoArgsConstructor to AuthResponse

--- src/main/java/com/soufiane/student_api/dto/AuthResponse.java
+++ src/main/java/com/soufiane/student_api/dto/AuthResponse.java
@@ -2,8 +2,10 @@ package com.soufiane.student_api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
     private String token;
